e372f7611e839d4f00b8ca9dd920f2ec657b013f
sophie lo <sophiee6077@gmail.com>
2024-03-14 03:09:45 +0800

    Initial Commit

---
+++ HowAreYou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HowAreYou
+//
+//  Created by Sophie Lo on 2024/3/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
